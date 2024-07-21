10df52d2d67636809ff671f2dc80a88bb15ca003
Loogeek <xiaolong3956@gmail.com>
2024-07-21 16:23:04 +0800

    feat: 新增支持ejs渲染动态模板数据

--- packages/init/lib/installTemplate.js
+++ packages/init/lib/installTemplate.js
@@ -2,10 +2,16 @@ import path from 'node:path'
 import fse from 'fs-extra'
 import { pathExistsSync } from 'path-exists'
 import ora from 'ora'
-import { log } from '@loogeek/cli-utils'
+import ejs from 'ejs'
+import { glob } from 'glob'
+import { log, makeInput, makeList, printErrorLog } from '@loogeek/cli-utils'
 
 function getCacheFilePath(targetPath, template) {
   return path.resolve(targetPath, 'node_modules', template.npmName, 'template')
+}
+
+function getCachePluginPath(targetPath, template) {
+  return path.resolve(targetPath, 'node_modules', template.npmName, 'plugins', 'index.js')
 }
 
 function copyFile(targetPath, template, installDir) {
@@ -17,6 +23,57 @@ function copyFile(targetPath, template, installDir) {
   })
   spinner.stop()
   log.success('模板拷贝成功！')
+}
+
+async function ejsRender(targetPath, installDir, template, name) {
+  log.verbose('ejsRender', installDir, template)
+  const { ignore = [] } = template
+
+  let data = {}
+  const pluginPath = getCachePluginPath(targetPath, template)
+  console.log(11111, ignore, targetPath, template, pluginPath)
+  if (pathExistsSync(pluginPath)) {
+    const pluginFn = (await import(pluginPath)).default
+    const api = {
+      makeList,
+      makeInput,
+    }
+
+    data = await pluginFn(api)
+  }
+
+  const ejsData = {
+    data: {
+      name, // 渲染package name名称
+      ...data,
+    },
+  }
+
+  try {
+    const files = await glob('**', {
+      cwd: installDir,
+      nodir: true,
+      ignore: [
+        ...ignore,
+        '**/node_modules/**',
+      ],
+    })
+
+    files?.forEach((file) => {
+      const filePath = path.join(installDir, file)
+      log.verbose('filePath', filePath)
+      ejs.renderFile(filePath, ejsData, (err, result) => {
+        if (!err)
+          fse.writeFileSync(filePath, result)
+        else
+          printErrorLog(err)
+      })
+    })
+    // })
+  }
+  catch (error) {
+    printErrorLog(error)
+  }
 }
 
 export default async function installTemplate(selectedTemplate, opts) {
@@ -42,4 +99,7 @@ export default async function installTemplate(selectedTemplate, opts) {
     fse.ensureDirSync(installDir)
   }
   copyFile(targetPath, template, installDir)
+
+  // 渲染ejs模板数据
+  await ejsRender(targetPath, installDir, template, name)
 }
